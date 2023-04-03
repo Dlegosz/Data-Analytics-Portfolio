37f554e4a4a4c2293bc5ae4f6fdc4b6960130a68
David <109626914+Dlegosz@users.noreply.github.com>
2023-04-03 13:27:18 -0400

    Update SQL File.sql

--- NYC Restaurant Food Order & Delivery Analysis/SQL File.sql
+++ NYC Restaurant Food Order & Delivery Analysis/SQL File.sql
@@ -1,6 +1,9 @@
 /*
-  First we want to get a glimps of the data we are working with. The Limit function will 
+  First we want to get a glimps of the data we are working with. The Limit function will only return 25 rows. This is convenient if our dataset is very large and we dont want to query a huge amount of data. 
+*/
 
 SELECT *
 FROM "Raw NYC Food & Delivery" 
 limit 25;
+
+
