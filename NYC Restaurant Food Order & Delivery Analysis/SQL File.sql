ee0cc9d4c30ee0f97104e7f26c4bbb27aaa5fd03
David <109626914+Dlegosz@users.noreply.github.com>
2023-04-04 15:30:38 -0400

    Update SQL File.sql

--- NYC Restaurant Food Order & Delivery Analysis/SQL File.sql
+++ NYC Restaurant Food Order & Delivery Analysis/SQL File.sql
@@ -6,4 +6,20 @@ SELECT *
 FROM "Raw NYC Food & Delivery" 
 limit 25;
 
+/*
+  Using COUNT DISTINCT on the restaurant_name we can get a sense as to how many unique restaurants we have in our dataset
+*/
 
+SELECT COUNT(DISTINCT(restaurant_name))
+FROM "Raw NYC Food & Delivery" 
+
+/* 
+  I noticed some restaurant names had non-alphabetical characters. To filter those out you would typically execute the following command:
+ */
+ 
+ SELECT REPLACE(restaurant_name, '[^a-zA-Z]', '') AS filtered_restaurant_names
+FROM "Raw NYC Food & Delivery"
+
+/*
+  But since 
+*/
